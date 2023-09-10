c10ed86b68ee5f81a8e993b859741d8a3ddec8a5
Dev Aryan Dogra <devdogra1@gmail.com>
2023-09-10 16:35:48 +0530

    Add debouncing to fetchAndRenderMovies

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,32 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import './App.css'
 import MovieCard from './components/Movie/Movie';
-
+// import { debounce } from 'lodash';
+import debounce from './utils/debounce';
 // DEBOUNCE AND THROTTLING NEEDED
 
 const API_URL = 'https://www.omdbapi.com?apikey=9f6a20f9'
 // const API_URL = 'https://www.omdbapi.com?apikey=9f6a20f9'
 
 function App() {
-
-    const movie = {
-        "Title": "Pulp Fiction",
-        "Year": "1994",
-        "imdbID": "tt0110912",
-        "Type": "movie",
-        "Poster": "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-    }
-
-
-
-    async function fetchMoviesBySearch(search) {
-        const response = await fetch(`${API_URL}&s=${search}`)
-        const data = await response.json()
-        return data;
-    }
-
     const [movieList, setMovieList] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
-    // fetchMoviesBySearch("Pulp Fiction").then(d => console.log(d)); 
+
+    async function fetchAndRenderMovies(query) {
+        const response = await fetch(`${API_URL}&s=${query}`)
+        const { Search } = await response.json()
+        setMovieList(Search);
+        console.log(Search);
+    }
+
+    const DELAY = 1000; 
+    const debouncedFetchAndRenderMovies = useCallback(debounce(fetchAndRenderMovies, DELAY), [])
+
+
     useEffect(() => {
-        fetchMoviesBySearch(searchTerm).then(data => {
-            setMovieList(data.Search)
-            console.log(data);
-        })
+       debouncedFetchAndRenderMovies(searchTerm);
     }, [searchTerm])
+
 
 
 
@@ -56,7 +49,7 @@ function App() {
                             <div className="movies-container">
                                 {
                                     movieList.map(movie => {
-                                        return <MovieCard movie={movie} />
+                                        return <MovieCard key={movie.imdbID} movie={movie} />
                                     })
                                 }
                             </div>
