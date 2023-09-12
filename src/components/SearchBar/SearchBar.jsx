b0bdcfda71f6444a89274141bfa84399c04d6a72
Dev Aryan Dogra <devdogra1@gmail.com>
2023-09-12 14:20:49 +0530

    Add animation to search bar (grow)

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -6,7 +6,10 @@ export default function SearchBar() {
         <input
             type="text"
             placeholder='Search for movies'
-            className='bg-white focus:opacity-70 text-black opacity-30 outline-none py-2 px-8 rounded-full'
+            className='bg-white focus:opacity-70 
+            transition-all ease-in-out duration-200
+            text-sm focus:text-lg
+            text-black opacity-30 outline-none py-2 px-8 rounded-full'
             // value={searchTerm}
             // onChange={e => setSearchTerm(e.target.value)}
         />
